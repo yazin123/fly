90ca5cec30a695922ec3781acc722c4b70d2d3a8
yazin-rhope <mohammedyazin4@gmail.com>
2024-10-29 14:50:34 +0530

    removed twitter from footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,7 +13,7 @@ import {
 
 const Footer = () => {
   const socialLinks = [
-    { icon: FaTwitter, href: '#', label: 'Twitter' },
+
     { icon: FaFacebook, href: '#', label: 'Facebook' },
     { icon: FaInstagram, href: '#', label: 'Instagram' },
     { icon: FaLinkedin, href: '#', label: 'LinkedIn' }
