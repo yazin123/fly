70a6c8bf06a1c965ce29d69e078c6df62c711ecd
yazin-rhope <mohammedyazin4@gmail.com>
2024-10-29 15:57:43 +0530

    added meta, changed address

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,9 +12,9 @@ import {
 
 const Footer = () => {
   const socialLinks = [
-    { icon: FaFacebook, href: '#', label: 'Facebook' },
-    { icon: FaInstagram, href: '#', label: 'Instagram' },
-    { icon: FaLinkedin, href: '#', label: 'LinkedIn' }
+    { icon: FaFacebook, href: 'https://www.facebook.com/forwardlearningyouth/', label: 'Facebook' },
+    { icon: FaInstagram, href: 'https://www.instagram.com/flynetwork.in/', label: 'Instagram' },
+    { icon: FaLinkedin, href: 'https://www.linkedin.com/company/forward-learning-youth/', label: 'LinkedIn' }
   ];
 
   const contactInfo = [
